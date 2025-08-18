310f043679705c837c1f273f425b0e69ed3f052e
Yilly <lyl020713@gmail.com>
2025-08-18 13:23:17 -0400

    initial commit

---
+++ src/main/java/com/yilly/lims/LogisticsInventoryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.yilly.lims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsInventoryManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticsInventoryManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yilly/lims/LogisticsInventoryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yilly.lims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsInventoryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
