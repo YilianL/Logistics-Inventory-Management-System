52a8daf9962d8fd8e0ef21cf7b4027959294248a
Yilly <lyl020713@gmail.com>
2025-08-21 21:00:09 -0400

    customerfeedback.java

--- src/main/java/com/yilly/lims/entity/Customer.java
+++ src/main/java/com/yilly/lims/entity/Customer.java
@@ -12,7 +12,7 @@ public class Customer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long customerID;
+    private String customerID;
 
     private String name;
 
---
+++ src/main/java/com/yilly/lims/entity/CustomerFeedback.java
@@ -0,0 +1,30 @@
+package com.yilly.lims.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "customer_feedback")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomerFeedback {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String feedbackID;
+
+    private String customerID;
+
+    private String sorderID;
+
+    private Integer rating;
+
+    private String feedbackText;
+
+    private LocalDateTime submittedTime;
+}
